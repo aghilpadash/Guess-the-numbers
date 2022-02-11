abb974959a7332134f40fa6804c4d63de7d975aa
aghilpadash <aghilpadash73@gmail.com>
2022-02-11 23:28:40 +0330

    add default style constant

---
+++ constant/default-styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    bodyText: {
+        fontFamily: "vazir",
+        color: 'red',
+    },
+    title: {
+        fontFamily: "vazir-bold",
+        fontSize: 18,
+        
+    }
+})
